2ac2075ce739f5529f74bf318ceed783c7f7c0fe
DevDyna <andrearisorti2003@gmail.com>
2023-09-17 23:14:11 +0200

    implemented tar.exe and rmdir like SD

--- VE packager.bat
+++ VE packager.bat
@@ -7,3 +7,30 @@ robocopy /E radiant-redstone\assets VE\assets
 robocopy /E better-nature\assets VE\assets
 ::copy banner
 robocopy /E vanilla-expansion VE
+
+mkdir zip
+::better-nature
+cd better-nature
+tar.exe -cf better-nature.zip assets pack.png pack.mcmeta
+robocopy . .. better-nature.zip /MOVE
+cd ..
+robocopy . zip better-nature.zip /MOVE
+::player-utilities
+cd player-utilities
+tar.exe -cf player-utilities.zip assets pack.png pack.mcmeta
+robocopy . .. player-utilities.zip /MOVE
+cd ..
+robocopy . zip player-utilities.zip /MOVE
+::radiant-redstone
+cd radiant-redstone
+tar.exe -cf radiant-redstone.zip assets pack.png pack.mcmeta
+robocopy . .. radiant-redstone.zip /MOVE
+cd ..
+robocopy . zip radiant-redstone.zip /MOVE
+::vanilla-expansion
+cd VE
+tar.exe -cf vanilla-expansion.zip assets pack.png pack.mcmeta
+robocopy . .. vanilla-expansion.zip /MOVE
+cd ..
+robocopy . zip vanilla-expansion.zip /MOVE
+rmdir /s /q VE
